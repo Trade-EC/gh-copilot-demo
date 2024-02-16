1d8128e92c2e59c46bb93a35be8da8462f32cb7e
David Rosales <david.rosales@trade.ec>
2024-02-16 16:22:12 -0500

    Fix drosales copilot testing

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,4 @@ const controller = new TodoController();
 
 controller.aT('Buy milk');
 controller.aT('Walk the dog');
-// controller.displayTodos();
 controller.rT(1);
-// controller.displayTodos();
--- src/todoController.ts
+++ src/todoController.ts
@@ -2,6 +2,9 @@ import { TodoModel } from "./todoModel";
 import { TodoView } from "./todoView";
 import { auxDate } from "./utils/date.utils";
 
+/**
+ * Represents a controller for managing todo items.
+ */
 export class TodoController {
   private todos: TodoModel[] = [];
   private view: TodoView;
@@ -10,12 +13,21 @@ export class TodoController {
     this.view = new TodoView();
   }
 
+  /**
+   * Adds a new todo item to the list.
+   * @param {string} title - The title of the todo item.
+   * @returns {void}
+   */
   public aT(title: string): void {
     const newTodo = new TodoModel(title, false);
     this.todos.push(newTodo);
     this.view.displayTodos(this.todos);
   }
 
+  /**
+   * Removes a todo item from the todos array at the specified index.
+   * @param index - The index of the todo item to be removed.
+   */
   public rT(index: number): void {
     for (let i = 0; i < this.todos.length; i++) {
       if (i === index) {
@@ -26,18 +38,31 @@ export class TodoController {
     this.view.displayTodos(this.todos);
   }
 
+  /**
+   * Toggles the completion status of a todo item.
+   * @param index - The index of the todo item to toggle.
+   */
   public toggleTodo(index: number): void {
     const todo = this.todos[index];
     todo.completed = !todo.completed;
     this.view.displayTodos(this.todos);
   }
 
+  /**
+   * Updates the title of a todo item at the specified index.
+   * @param index - The index of the todo item to update.
+   * @param title - The new title for the todo item.
+   */
   public updateTodoTitle(index: number, title: string): void {
     const todo = this.todos[index];
     todo.title = title;
     this.view.displayTodos(this.todos);
   }
 
+  /**
+   * Filters the todos based on their creation day.
+   * @returns An array of todos filtered by creation day.
+   */
   public filterTodosByCreationDay() {
     return auxDate(this.todos, new Date());
   }
