95b3464b27a3457c8096c2f5b20c2f59e706769c
AlexisH <alexis.huayna@trade.ec>
2024-02-16 16:20:46 -0500

    Add and remove todo functionality

--- src/todoController.ts
+++ src/todoController.ts
@@ -10,12 +10,21 @@ export class TodoController {
     this.view = new TodoView();
   }
 
+  /**
+   * Adds a new todo item to the list.
+   * @param {string} title - The title of the todo item.
+   * @returns {void}
+   */
   public aT(title: string): void {
     const newTodo = new TodoModel(title, false);
     this.todos.push(newTodo);
     this.view.displayTodos(this.todos);
   }
 
+  /**
+   * Removes a todo item from the todos array at the specified index.
+   * @param index - The index of the todo item to be removed.
+   */
   public rT(index: number): void {
     for (let i = 0; i < this.todos.length; i++) {
       if (i === index) {
