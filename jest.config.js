a4f50cd13815a9768ef12d001489e3640748ba40
Alexander Tigselema <alexxtp@gmail.com>
2024-02-15 23:18:18 -0500

    Add Jest configuration and update package.json

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+  ],
+  testMatch: ['<rootDir>/**/*.test.ts'], // Add the test path here
+};
